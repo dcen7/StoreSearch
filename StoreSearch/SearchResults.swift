d158837e2c7063637e482d93e0d78221e9173eb3
dcen7 <mdcengiz@gmail.com>
2022-12-08 14:02:49 -0600

    Add a search bar and table view

---
+++ StoreSearch/SearchResults.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResults.swift
+//  StoreSearch
+//
+//  Created by Mehmet Deniz Cengiz on 12/8/22.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
