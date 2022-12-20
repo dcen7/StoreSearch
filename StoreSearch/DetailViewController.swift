875f3287e27a39f4badc59b2cde155ddd8a868d2
dcen7 <mdcengiz@gmail.com>
2022-12-20 11:04:44 -0600

    Present detail view modally

---
+++ StoreSearch/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Mehmet Deniz Cengiz on 12/20/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
