7dbda849504cb58ff876b7bbf3708d5205e08854
dcen7 <mdcengiz@gmail.com>
2022-12-21 13:22:09 -0600

    Compete detail pop-up view

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -170,6 +170,8 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         performSegue(withIdentifier: "ShowDetail", sender: indexPath)
+        
+        
     }
     
     func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
@@ -177,6 +179,16 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             return nil
         } else {
             return indexPath
+        }
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            let detailViewController = segue.destination as! DetailViewController
+            
+            let indexPath = sender as! IndexPath
+            detailViewController.searchResult = searchResults[indexPath.row]
         }
     }
 }
